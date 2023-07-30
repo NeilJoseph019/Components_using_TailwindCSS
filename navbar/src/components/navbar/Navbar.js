d9918d1f788504506b85a8418c1da1644f73e49d
Neil Joseph <neiljoseph@Neils-MacBook-Pro.local>
2023-07-30 09:25:00 +0100

    Responsive navbar completed.

--- navbar/src/components/navbar/Navbar.js
+++ navbar/src/components/navbar/Navbar.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './navbar.module.css'
+import { FaBars } from 'react-icons/fa6'
 
 const Navbar = () => {
+
+    const [hidden, setHidden] = useState(true)
+
+    const burgerClick = () =>{
+        setHidden(prev => !prev)
+    }
+
   return (
     
         <nav>   
@@ -15,7 +23,7 @@ const Navbar = () => {
                     </div>
 
                     {/* Home Nav */}
-                    <div className={styles.home}>
+                    <div >
                         <button>Home</button>
                     </div>
                 </div>
@@ -36,9 +44,33 @@ const Navbar = () => {
                     </div>
                 </div>
 
+                <div>
+                    <button 
+                        onClick={burgerClick} 
+                        className={styles.tabs_bars} >
+                            <FaBars/>
+                    </button>
+                </div>
+
             </div>
        
          {/* Mobile Nav */}
+        <div className={`${styles.tabs_mobile_view} ${hidden ? 'hidden':''}`}>
+                {/* Tabs mobile view */}
+            <div>
+                <button>Tab 1</button>
+            </div>
+            <div>
+                <button>Tab 2</button>
+            </div>
+            <div>
+                <button>Tab 3</button>
+            </div>
+            <div>
+                <button>Tab 4</button>
+            </div>
+        </div>
+
         </nav>
   )
 }
